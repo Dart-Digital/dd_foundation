38847d5287e9c58556af8127593cf3b6b3572a76
Nick Schoonens <nick@schoonzie.com>
2016-04-24 17:44:50 +1000

    Adding interchange template.

---
+++ dd-foundation-img-interchange.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * DD Foundation IMG Interchange.
+ */
+?>
+<img data-interchange="[<?php print $small; ?>, small], [<?php print $medium; ?>, medium], [<?php print $large; ?>, large]" alt="<?php print $alt; ?>" title="<?php print $title; ?>">
+<noscript><img src="<?php print $large; ?>" alt="<?php print $alt; ?>" title="<?php print $title; ?>"></noscript>
